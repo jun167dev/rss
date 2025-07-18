1e2a1cf601145c187d810af4654ea3cbd9347d0a
junH176 <johnnywaver@gmail.com>
2025-07-18 23:08:16 +0900

    [del]pdate-rss.ps1

--- update-rss.ps1
+++
@@ -1,9 +0,0 @@
-$ua = "Mozilla/5.0"
-$url = "https://feeds.tbs.co.jp/rss/tbsradio/TS954.xml?limit=100"
-$output = "rss/chiki.xml"
-
-Invoke-WebRequest -Uri $url -UserAgent $ua -OutFile $output
-
-git add rss/chiki.xml
-git commit -m "Update Chiki RSS" -ErrorAction SilentlyContinue
-git push
